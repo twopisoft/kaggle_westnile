b6eaca41b874617daa282355d5617b4880f8487a
twopisoft <twopisoft@gmail.com>
2015-05-20 10:00:24 +0800

    0.76767/0.75403 lb/local

--- python/kaggle_script.py
+++ python/kaggle_script.py
@@ -7,90 +7,64 @@ __author__ : Abhihsek
 import pandas as pd
 import numpy as np
 from sklearn import ensemble, preprocessing
+import tune
 
-# Load dataset 
-train = pd.read_csv('../input/train.csv')
-test = pd.read_csv('../input/test.csv')
-sample = pd.read_csv('../input/sampleSubmission.csv')
-weather = pd.read_csv('../input/weather.csv')
 
-# Get labels
-labels = train.WnvPresent.values
 
-# Not using codesum for this benchmark
-weather = weather.drop('CodeSum', axis=1)
+def prep_test_data(train, test, weather):
 
-# Split station 1 and 2 and join horizontally
-weather_stn1 = weather[weather['Station']==1]
-weather_stn2 = weather[weather['Station']==2]
-weather_stn1 = weather_stn1.drop('Station', axis=1)
-weather_stn2 = weather_stn2.drop('Station', axis=1)
-weather = weather_stn1.merge(weather_stn2, on='Date')
+    test['year'] = test.Date.apply(tune.create_year)
+    test['month'] = test.Date.apply(tune.create_month)
+    test['day'] = test.Date.apply(tune.create_day)
 
-# replace some missing values and T with -1
-weather = weather.replace('M', -1)
-weather = weather.replace('-', -1)
-weather = weather.replace('T', -1)
-weather = weather.replace(' T', -1)
-weather = weather.replace('  T', -1)
+    test['Lat_int'] = test.Latitude.apply(int)
+    test['Long_int'] = test.Longitude.apply(int)
 
-# Functions to extract month and day from dataset
-# You can also use parse_dates of Pandas.
-def create_month(x):
-    return x.split('-')[1]
+    test = test.drop(['Id', 'Address', 'AddressNumberAndStreet'], axis = 1)
 
-def create_day(x):
-    return x.split('-')[2]
+    test = test.merge(weather, on='Date')
+    test = test.drop(['Date'], axis = 1)
 
-train['month'] = train.Date.apply(create_month)
-train['day'] = train.Date.apply(create_day)
-test['month'] = test.Date.apply(create_month)
-test['day'] = test.Date.apply(create_day)
+    lbl = preprocessing.LabelEncoder()
+    lbl.fit(list(train['Species'].values) + list(test['Species'].values))
+    train['Species'] = lbl.transform(train['Species'].values)
+    test['Species'] = lbl.transform(test['Species'].values)
 
-# Add integer latitude/longitude columns
-train['Lat_int'] = train.Latitude.apply(int)
-train['Long_int'] = train.Longitude.apply(int)
-test['Lat_int'] = test.Latitude.apply(int)
-test['Long_int'] = test.Longitude.apply(int)
+    lbl.fit(list(train['Street'].values) + list(test['Street'].values))
+    train['Street'] = lbl.transform(train['Street'].values)
+    test['Street'] = lbl.transform(test['Street'].values)
 
-# drop address columns
-train = train.drop(['Address', 'AddressNumberAndStreet','WnvPresent', 'NumMosquitos'], axis = 1)
-test = test.drop(['Id', 'Address', 'AddressNumberAndStreet'], axis = 1)
+    lbl.fit(list(train['Trap'].values) + list(test['Trap'].values))
+    train['Trap'] = lbl.transform(train['Trap'].values)
+    test['Trap'] = lbl.transform(test['Trap'].values)
 
-# Merge with weather data
-train = train.merge(weather, on='Date')
-test = test.merge(weather, on='Date')
-train = train.drop(['Date'], axis = 1)
-test = test.drop(['Date'], axis = 1)
+    test = test.ix[:,(test != -1).any(axis=0)]
 
-# Convert categorical data to numbers
-lbl = preprocessing.LabelEncoder()
-lbl.fit(list(train['Species'].values) + list(test['Species'].values))
-train['Species'] = lbl.transform(train['Species'].values)
-test['Species'] = lbl.transform(test['Species'].values)
+    return test
 
-lbl.fit(list(train['Street'].values) + list(test['Street'].values))
-train['Street'] = lbl.transform(train['Street'].values)
-test['Street'] = lbl.transform(test['Street'].values)
+def main():
 
-lbl.fit(list(train['Trap'].values) + list(test['Trap'].values))
-train['Trap'] = lbl.transform(train['Trap'].values)
-test['Trap'] = lbl.transform(test['Trap'].values)
+    train_df = pd.read_csv('../input/train.csv')
+    test_df = pd.read_csv('../input/test.csv')
+    weather_df = pd.read_csv('../input/weather.csv')
 
-# drop columns with -1s
-train = train.ix[:,(train != -1).any(axis=0)]
-test = test.ix[:,(test != -1).any(axis=0)]
+    weather = tune.prep_weather_data(weather_df)
 
-#train.to_csv('mod_train.csv',index=False)
+    train = tune.prep_train_data(train_df, test_df, weather)
 
-# Random Forest Classifier 
-#clf = ensemble.RandomForestClassifier(n_jobs=-1, n_estimators=1000, min_samples_split=1)
+    test = prep_test_data(train, test_df, weather)
 
-# GBM classifier
-clf = ensemble.GradientBoostingClassifier(loss='exponential', n_estimators=100, min_samples_split=1)
-clf.fit(train, labels)
+    sample = pd.read_csv('../input/sampleSubmission.csv')
 
-# create predictions and submission file
-predictions = clf.predict_proba(test)[:,1]
-sample['WnvPresent'] = predictions
-sample.to_csv('../output/gbm_100_exp.csv', index=False)
+    # GBM classifier
+    clf = ensemble.GradientBoostingClassifier(loss='exponential', n_estimators=100, min_samples_split=1)
+
+    clf = tune.train_classifier(clf, train, ['2007','2009','2011','2013'])
+
+    # create predictions and submission file
+    predictions = clf.predict_proba(test)[:,1]
+    sample['WnvPresent'] = predictions
+    sample.to_csv('../output/gbm_100_exp_2.csv', index=False)
+
+if __name__ == "__main__":
+    main()
